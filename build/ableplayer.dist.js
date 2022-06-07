799193af0b6552e461a4d2ec7042d5a14a9dcb76
Matthew Shubat <matthew.shubat@jonahgroup.com>
2022-06-07 17:50:56 -0400

    522: slider tooltips dismissible via escape key
    (cherry picked from commit 65d41a33ddde6ec688ba0eec64b40d83b18b8240)

--- build/ableplayer.dist.js
+++ build/ableplayer.dist.js
@@ -6525,6 +6525,12 @@ if (thisObj.useTtml && (trackSrc.endsWith('.xml') || trackText.startsWith('<?xml
 		}
 	};
 
+	AccessibleSlider.prototype.hideSliderTooltips = function () {
+		this.overHead = false;
+		this.overBody = false;
+		this.refreshTooltip();
+	};
+
 	AccessibleSlider.prototype.setTooltipPosition = function (x) {
 		this.timeTooltip.css({
 			left: x - (this.timeTooltip.width() / 2) - 10,
@@ -12033,6 +12039,7 @@ if (thisObj.useTtml && (trackSrc.endsWith('.xml') || trackText.startsWith('<?xml
 			if (which === 27) { // escape
 				this.closePopups();
 				this.$tooltipDiv.hide();
+				this.seekBar.hideSliderTooltips();
 			}
 			else if (which === 32) { // spacebar = play/pause
 				// disable spacebar support for play/pause toggle as of 4.2.10
